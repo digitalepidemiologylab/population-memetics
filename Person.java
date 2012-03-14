69b14a2f6843bc70d28b563d8432558de627c557
Marcel Salathe <salathe.marcel@gmail.com>
2012-03-13 21:35:04 -0400

    skeleton of classes

--- Person.java
+++ Person.java
@@ -1,4 +1,19 @@
 package com.salathe.populationmemetics;
 
+
 public class Person {
+
+    private String id;
+
+
+
+    public Person(String id) {
+        this.id = id;
+    }
+
+    public String toString() {
+        return this.id;
+    }
+
+
 }
--- SimulationSettings.java
+++ SimulationSettings.java
@@ -1,5 +1,45 @@
 package com.salathe.populationmemetics;
 
-
 public class SimulationSettings {
+
+    private static SimulationSettings ourInstance = new SimulationSettings();
+
+
+    private int numberOfPeople = 2000;
+    private int k = 10;
+    private double rewiringProbability = 0.02;
+
+
+    public static SimulationSettings getInstance() {
+        return ourInstance;
+    }
+
+    private SimulationSettings() {
+    }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public void setNumberOfPeople(int numberOfPeople) {
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public void setK(int k) {
+        this.k = k;
+    }
+
+    public double getRewiringProbability() {
+        return rewiringProbability;
+    }
+
+    public void setRewiringProbability(double rewiringProbability) {
+        this.rewiringProbability = rewiringProbability;
+    }
+
+
 }
