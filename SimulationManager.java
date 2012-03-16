4bdb5edc73ee278d3692a6b85d8c011f070b8c76
Marcel Salathe <salathe.marcel@gmail.com>
2012-03-15 22:51:39 -0400

    minor edits

--- SimulationManager.java
+++ SimulationManager.java
@@ -8,8 +8,8 @@ public class SimulationManager {
     }
 
     private void run() {
-        int repeatsPerP = 1;
-        double[] ps = {0.01, 0.011, 0.013, 0.017, 0.023, 0.04, 0.1, 0.11, 0.13, 0.17, 0.23, 0.4};
+        int repeatsPerP = 10;
+        double[] ps = {0.01, 0.011, 0.013, 0.017, 0.023, 0.031, 0.04, 0.1, 0.11, 0.13, 0.17, 0.23, 0.31, 0.4};
         for (int i = 0; i < ps.length; i++) {
             double sumTimesteps = 0;
             SimulationSettings.getInstance().setRewiringProbability(ps[i]);
