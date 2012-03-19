09aab3af0254ca4217404e73b63697c3f563b461
Marcel Salathe <salathe.marcel@gmail.com>
2012-03-18 21:31:43 -0400

    generalize model

--- SimulationManager.java
+++ SimulationManager.java
@@ -1,15 +1,39 @@
 package com.salathe.populationmemetics;
 
+
 import java.util.ArrayList;
 
 public class SimulationManager {
 
     public static void main(String[] args) {
         SimulationManager sm = new SimulationManager();
-        sm.runStandardModelComplexContagion();
+//        sm.runStandardModelComplexContagion();
+        sm.runBidrectionalModelComplexContagion();
+    }
+
+    private void runBidrectionalModelComplexContagion() {
+        // the network
+        SimulationSettings.getInstance().setNumberOfPeople(50*50);
+        SimulationSettings.getInstance().setGraphType(Simulation.GRAPH_TYPE_WATTS_STROGATZ_1D);
+        SimulationSettings.getInstance().setRewiringType(Simulation.EDGE_REWIRING);
+        SimulationSettings.getInstance().setK(8);
+        SimulationSettings.getInstance().setRewiringProbability(0.01);
+        // the settings
+        SimulationSettings.getInstance().setTransmissionRate00(0.1);
+        SimulationSettings.getInstance().setTransmissionRate01(0.0);
+        SimulationSettings.getInstance().setTransmissionRate11(0.1);
+        SimulationSettings.getInstance().setViability00(1.0);
+        SimulationSettings.getInstance().setViability00(0.0);
+        SimulationSettings.getInstance().setViability00(1.0);
+        SimulationSettings.getInstance().setMemotype_exposing_to_exposed(new String[][]{{"00"},{"00"},{"01"},{"00"},{"01"},{"11"},{"01"},{"11"},{"11"}});
+
+        Simulation simulation = new Simulation();
+        simulation.run();
     }
 
     private void runStandardModelComplexContagion() {
+        // MAKE SURE YOU CALL setRandomNeighboursTo11 in Simulation!
+        // ALSO: CALL checkWhetherToStop() in Simulation!
         SimulationSettings.getInstance().setNumberOfPeople(200*200);
         SimulationSettings.getInstance().setGraphType(Simulation.GRAPH_TYPE_WATTS_STROGATZ_2D);
         SimulationSettings.getInstance().setRewiringType(Simulation.EDGE_REWIRING);
